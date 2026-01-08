a10e542f5712a03388df743d067c4b28b64fe283
hemant17 <hemantbolashetti77@gmail.com>
2026-01-08 15:35:48 +0530

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -1,19 +1,15 @@
 from library import library_details
 
 def test_library_details():
-    # Define test data
-    book_id = 101
+    id = 101
     title = "The Great Gatsby"
     name = "F. Scott Fitzgerald"
     year = 1925
     
-    # Define the expected single string output
     expected_output = (
-        f"Book Id:{book_id}\n"
+        f"Book Id:{id}\n"
         f"Book title:{title}\n"
         f"Author name:{name}\n"
         f"Published year:{year}"
     )
-    
-    # Assert that the function return matches the expected string
-    assert library_details(book_id, title, name, year) == expected_output
+    assert library_details(id, title, name, year) == expected_output
