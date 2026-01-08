15d3430b6e93bc45cdad26d16015efb8585e8aec
hemant17 <hemantbolashetti77@gmail.com>
2026-01-08 16:15:15 +0530

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -2,15 +2,15 @@
 from library import library_details 
 
 def test_library_details():
-    id = 101
-    title = "The Great Gatsby"
-    name = "F. Scott Fitzgerald"
-    year = 1925
+    id =101
+    title ="The Great Gatsby"
+    name ="F. Scott Fitzgerald"
+    year =1925
     
     expected_output = (
-        f"Book Id:{id}\n"
-        f"Book title:{title}\n"
-        f"Author name:{name}\n"
-        f"Published year:{year}" 
+        "Book Id:{id}\n"
+        "Book title:{title}\n"
+        "Author name:{name}\n"
+        "Published year:{year}" 
     )
     assert library_details(id, title, name, year) == expected_output
