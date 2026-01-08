901a2e51a2f37cdf16e516c3565786e5baa113f9
hemant17 <hemantbolashetti77@gmail.com>
2026-01-08 16:52:21 +0530

    Update library.py

--- library.py
+++ library.py
@@ -3,14 +3,14 @@ def library_details(id, title, name, year):
     result = (
         f"Book Id:{id}\n"
         f"Book title:{title}\n"
-        f"Authorname:{name}\n"
-        f"Published year:{year}\n"
+        f"Authorname:{aname}\n"
+        f"Published year:{pyear}\n"
     )
     return result
 
 if __name__ == "__main__":
     id =101
     title ="devops"
-    name ="hary"
-    year =1925
-    print(library_details(id,title,name,year))
+    aname ="hary"
+    pyear =1925
+    print(library_details(id,title,aname,pyear))
