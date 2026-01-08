ed5870f054b244e052b156c946ff325e5233ce30
hemant17 <hemantbolashetti77@gmail.com>
2026-01-08 16:48:44 +0530

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -4,13 +4,13 @@ from library import library_details
 def test_library_details():
     id =101
     title ="devops"
-    name ="alice"
+    name ="hary"
     year =1925
     
     expected_output = (
         "Book Id:{id}\n"
         "Book title:{title}\n"
-        "Author name:{name}\n"
+        "Authorname:{name}\n"
         "Publishedyear:{year}" 
     )
-    assert (library_details(101,"devops","alice",1925)) == expected_output
+    assert (library_details(101,"devops","hary",1925)) == expected_output
